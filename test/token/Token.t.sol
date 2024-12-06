34adb078dcc76bb3d832d731b1de40cf6d0a241b
ali <abdolazim35@yahoo.com>
2024-12-06 22:45:04 +0330

    add test

--- test/token/Token.t.sol
+++ test/token/Token.t.sol
@@ -1,59 +1,95 @@
-// Test file
-pragma solidity ^0.8.23;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "../../src/token/ImagineToken.sol"; // Import the contract
-import "../../src/token/IImagineToken.sol"; // Import the interface
+import {Test, console} from "forge-std/Test.sol";
+import {ImagineToken} from "../../src/token/ImagineToken.sol"; // Replace with the correct path
+import {IImagineToken} from "../../src/token/IImagineToken.sol"; // Import interface if needed
+import {MockUniswapV2Router} from "../../src/utils/external/MockUniswapV2Router.sol"; // Import the Mock Uniswap Router
 
 contract ImagineTokenTest is Test {
     ImagineToken public token;
-    address private constant OWNER = address(1);
-    address private constant NON_OWNER = address(2);
+    MockUniswapV2Router public mockUniswapV2Router;
 
-    // Import the ConstructorParams struct directly from IImagineToken interface
-    // You don't need to redefine it in the test anymore
+    string private constant INITIAL_URI = "https://initial-uri.com";
+    string private constant UPDATED_URI = "https://updated-uri.com";
 
-    string private constant TOKEN_NAME = "ImagineToken";
-    string private constant TOKEN_SYMBOL = "IMT";
-    string private constant TOKEN_URI = "https://initial-token-uri.com";
-    uint256 private constant INITIAL_SUPPLY = 1000000 * 10 ** 18;
-    address private constant TREASURY = address(3);
-    address private constant UNIV2ROUTER = address(4);
-    address private constant DEXTREASURY = address(5);
-    uint256 private constant FEE_BASIS_POINTS = 100; // Example fee
-    uint256 private constant DEX_FEE_BASIS_POINTS = 100;
-    uint256 private constant MIGRATION_FEE = 10;
-    uint256 private constant POOL_CREATION_FEE = 50;
-    uint256 private constant MC_LOWER_LIMIT = 1;
-    uint256 private constant MC_UPPER_LIMIT = 100;
-    uint256 private constant TOKENS_MIGRATION_THRESHOLD = 1000;
+    address private treasury = 0xc02aAA39a2A6d2B5321D1101c1b0C92a2b342F9a;
+    address private dexTreasury = 0xc02aAA39a2A6d2B5321D1101c1b0C92a2b342F9a;
+    address private factory = 0xc02aAA39a2A6d2B5321D1101c1b0C92a2b342F9a;
+    address private WETH = 0xc02aAA39a2A6d2B5321D1101c1b0C92a2b342F9a; // WETH mainnet address (example)
+    uint private totalSupply = 1000000 * 10 ** 18;
 
+    string private tokenName = "ImagineToken";
+    string private tokenSymbol = "IMT";
+    uint256 private initialTokenSupply = 1000000 * 10 ** 18;
+    uint256 private initialCollateralReserves = 1000 ether;
+    uint256 private initialVirtualTokenReserves = 1000000 * 10 ** 18;
+    uint256 private feeBasisPoints = 200; // 2% fee
+    uint256 private dexFeeBasisPoints = 50; // 0.5% dex fee
+    address private creator = address(this);
+    address private treasuryAddress =
+        0xc02aAA39a2A6d2B5321D1101c1b0C92a2b342F9a;
+    address private dexTreasuryAddress =
+        0xc02aAA39a2A6d2B5321D1101c1b0C92a2b342F9a;
+    uint256 private migrationFeeFixed = 10;
+    uint256 private poolCreationFee = 10;
+    uint256 private mcLowerLimit = 100;
+    uint256 private mcUpperLimit = 500;
+    uint256 private tokensMigrationThreshold = 1000000;
     function setUp() public {
-        // Use the ConstructorParams struct from the interface
+        // Deploy Mock Uniswap Router contract with the factory and WETH address
+        mockUniswapV2Router = new MockUniswapV2Router();
+
+        // // Setup the token contract, passing the address of the mock router
         IImagineToken.ConstructorParams memory params = IImagineToken
             .ConstructorParams({
-                name: TOKEN_NAME,
-                symbol: TOKEN_SYMBOL,
-                tokenURI: TOKEN_URI,
-                creator: OWNER,
-                totalSupply: INITIAL_SUPPLY,
-                virtualTokenReserves: 0,
-                virtualCollateralReserves: 0,
-                feeBasisPoints: FEE_BASIS_POINTS,
-                dexFeeBasisPoints: DEX_FEE_BASIS_POINTS,
-                migrationFeeFixed: MIGRATION_FEE,
-                poolCreationFee: POOL_CREATION_FEE,
-                mcLowerLimit: MC_LOWER_LIMIT,
-                mcUpperLimit: MC_UPPER_LIMIT,
-                tokensMigrationThreshold: TOKENS_MIGRATION_THRESHOLD,
-                treasury: TREASURY,
-                uniV2Router: UNIV2ROUTER,
-                dexTreasury: DEXTREASURY
+                name: tokenName,
+                symbol: tokenSymbol,
+                tokenURI: INITIAL_URI,
+                creator: creator,
+                totalSupply: initialTokenSupply,
+                virtualCollateralReserves: initialCollateralReserves,
+                virtualTokenReserves: initialVirtualTokenReserves,
+                feeBasisPoints: feeBasisPoints,
+                dexFeeBasisPoints: dexFeeBasisPoints,
+                migrationFeeFixed: migrationFeeFixed,
+                poolCreationFee: poolCreationFee,
+                mcLowerLimit: mcLowerLimit,
+                mcUpperLimit: mcUpperLimit,
+                tokensMigrationThreshold: tokensMigrationThreshold,
+                treasury: treasuryAddress,
+                dexTreasury: dexTreasuryAddress,
+                uniV2Router: address(mockUniswapV2Router)
             });
 
-        // Deploy the contract with the ConstructorParams struct from the interface
         token = new ImagineToken(params);
     }
 
-    // Test functions follow...
+    function test_checkConstructorData() public {
+        assertEq(token.name(), tokenName);
+        assertEq(token.symbol(), tokenSymbol);
+        assertEq(token.getTokenURI(), INITIAL_URI);
+        assertEq(token.creator(), creator);
+        assertEq(token.totalSupply(), initialTokenSupply);
+        assertEq(token.virtualCollateralReserves(), initialCollateralReserves);
+        assertEq(token.virtualTokenReserves(), initialVirtualTokenReserves);
+        assertEq(token.feeBPS(), feeBasisPoints);
+        assertEq(token.dexFeeBPS(), dexFeeBasisPoints);
+        assertEq(token.fixedMigrationFee(), migrationFeeFixed);
+        assertEq(token.poolCreationFee(), poolCreationFee);
+        assertEq(token.mcLowerLimit(), mcLowerLimit);
+        assertEq(token.mcUpperLimit(), mcUpperLimit);
+        assertEq(token.tokensMigrationThreshold(), tokensMigrationThreshold);
+        assertEq(token.treasury(), treasuryAddress);
+        assertEq(token.dexTreasury(), dexTreasuryAddress);
+
+        console.log(address(mockUniswapV2Router));
+        // console.log()
+        // assertEq(token.uniswapV2Router(), address(mockUniswapV2Router)); // Check mock Uniswap router
+    }
+
+    function test_SetTokenURI() public {
+        token.setTokenURI(UPDATED_URI);
+        assertEq(token.getTokenURI(), UPDATED_URI);
+    }
 }
