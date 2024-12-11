a8f6dae39609e22bbc465395f3850788b6d8a02f
Mehdi Gorbanzadeh <M.Gorbanzadeh@parlar.com>
2024-12-11 16:59:25 +0330

    add web (#29)

---
+++ script/deploy/ImagineFactory.deploy.s.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "forge-std/Script.sol";
+import "./../../contracts/release/extensions/imagine-factory/ImagineFactory.sol";
+import "forge-std/console.sol";
+
+contract DeployImagineFactory is Script {
+
+    function run() external returns (address) {
+
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        
+        uint256 totalSupply = 1_000_000_000 ether;
+        uint256 virtualTokenReserves = 1_060_000_000 ether;
+        uint256 virtualCollateralReserves = 1.6 ether;
+        uint256 feeBasisPoints = 100;
+        uint256 dexFeeBasisPoints = 6000;
+        uint256 migrationFeeFixed = 0.1 ether;
+        uint256 poolCreationFee = 0.05 ether;
+        uint256 mcUpperLimit = 27 ether;
+        uint256 mcLowerLimit = 25 ether;
+        uint256 tokensMigrationThreshold = 799538870462404697804703491; // near to 800 million ether
+        
+        address treasury = 0x1234567890abcdef1234567890abcdef12345678;
+        address dexTreasury = 0xabcdef1234567890abcdef1234567890abcdef12;
+        address uniswapV2Router = 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24;
+        address signer = 0x4567890abcdef1234567890abcdef1234567890ab;
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        ImagineFactory imagineFactory = new ImagineFactory(
+            totalSupply,
+            virtualTokenReserves,
+            virtualCollateralReserves,
+            feeBasisPoints,
+            dexFeeBasisPoints,
+            migrationFeeFixed,
+            poolCreationFee,
+            mcUpperLimit,
+            mcLowerLimit,
+            tokensMigrationThreshold,
+            treasury,
+            dexTreasury,
+            uniswapV2Router,
+            signer
+        );
+
+        vm.stopBroadcast();
+
+        return (address(imagineFactory));
+    }
+}
